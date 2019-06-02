2641bf5fe3ef480ebe4308a95db8b39de545a58d
Peter Malecki <pmalecki@peters-mbp.mynet>
2019-06-02 21:50:07 +0100

    Added files

---
+++ Messenger.py
@@ -0,0 +1,32 @@
+import sys
+import random
+from PySide2.QtWidgets import (QApplication, QLabel, QPushButton,
+                               QVBoxLayout, QWidget)
+
+from PySide2.QtWebEngineWidgets import ( QWebEngineView)
+from PySide2.QtCore import Slot, Qt
+
+class MyWidget(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+
+        self.hello = ["Hallo Welt", "你好，世界", "Hei maailma",
+            "Hola Mundo", "Привет мир"]
+
+        self.webView = QWebEngineView()
+        self.webView.load("https://www.messenger.com/login")
+
+        self.layout = QVBoxLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.addWidget(self.webView)
+        self.setLayout(self.layout)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec_())
